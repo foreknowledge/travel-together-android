61a84ece37ce785fd3f695f4922e2aa7a6212bd8
s2choco <kyj_0129@naver.com>
2019-11-07 23:56:23 +0900

    카카오 로그인 2차 구현 (modified 추가) - 스플래시 화면 & 로그인 화면 UI 완성

--- app/src/main/java/com/mungziapp/traveltogether/Activity/LoginActivity.java
+++ app/src/main/java/com/mungziapp/traveltogether/Activity/LoginActivity.java
@@ -18,15 +18,37 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Button btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
         Button btnKakaoLogin = findViewById(R.id.btn_kakao_login);
         btnKakaoLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Session session = Session.getCurrentSession();
-
                 session.addCallback(new SessionCallback());
-
                 session.open(AuthType.KAKAO_TALK, LoginActivity.this);
+            }
+        });
+
+        Button btnForgotPwd = findViewById(R.id.btn_forgot_pwd);
+        btnForgotPwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button btnGoSignUp = findViewById(R.id.btn_go_sign_up);
+        btnGoSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
