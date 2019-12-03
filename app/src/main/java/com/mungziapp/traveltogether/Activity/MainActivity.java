54cb768d0b72f996c0fd3610b44800d77029821d
s2choco <kyj_0129@naver.com>
2019-12-04 03:05:59 +0900

    메인화면 search bar 구현 #10 메인화면 스크롤 시 tool bar 동작 구현 #2

--- app/src/main/java/com/mungziapp/traveltogether/Activity/MainActivity.java
+++ app/src/main/java/com/mungziapp/traveltogether/Activity/MainActivity.java
@@ -1,9 +1,17 @@
 package com.mungziapp.traveltogether.Activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
@@ -26,6 +34,8 @@ public class MainActivity extends BaseActivity {
 
     private FragmentManager fm;
 
+    private EditText editSearch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,10 +43,15 @@ public class MainActivity extends BaseActivity {
 
         fm = getSupportFragmentManager();
 
+        setActionBarSlide();
         setAdapters();
         setTabBar();
         setAddTravelRoomButton();
         setSettingsButton();
+        setSearchButton();
+    }
+
+    private void setActionBarSlide() {
     }
 
     private void setAdapters() {
@@ -157,5 +172,50 @@ public class MainActivity extends BaseActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void setSearchButton() {
+        editSearch = findViewById(R.id.search_travel);
+        final Button btnClear = findViewById(R.id.btn_clear);
+
+        editSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                btnClear.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editSearch.getText().toString().equals("")) btnClear.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        editSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEARCH) {
+                    performSearch();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editSearch.setText("");
+            }
+        });
+    }
+
+    private void performSearch() {
+        editSearch.clearFocus();
+        InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (in != null)
+            in.hideSoftInputFromWindow(editSearch.getWindowToken(), 0);
     }
 }
