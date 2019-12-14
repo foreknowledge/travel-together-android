f5dec5ae02487a925ae9971de8b743c084386c7b
s2choco <kyj_0129@naver.com>
2019-12-14 18:55:14 +0900

    프로필 편집 구현 #13

--- app/src/main/java/com/mungziapp/traveltogether/activity/SettingsActivity.java
+++ app/src/main/java/com/mungziapp/traveltogether/activity/SettingsActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.kakao.network.ErrorResult;
 import com.kakao.usermgmt.UserManagement;
@@ -13,16 +16,60 @@ import com.kakao.usermgmt.callback.LogoutResponseCallback;
 import com.kakao.usermgmt.callback.UnLinkResponseCallback;
 import com.mungziapp.traveltogether.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class SettingsActivity extends BaseActivity {
 	private static final String TAG = "Settings :: ";
+	private static final int NORMAL_MODE = 0;
+	private static final int EDIT_MODE = 1;
+
+	private int mode = NORMAL_MODE;
+
+	private Button btnEdit;
+	private Button btnGoBefore;
+	private Button btnCancel;
+	private LinearLayout buttons;
+
+	private TextView profileName;
+	private TextView profileMessage;
+	private EditText editName;
+	private EditText editMessage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
 
+		setButtons();
+		init();
+	}
 
-		Button btnGoBefore = findViewById(R.id.btn_go_before);
+	private void init() {
+		profileName.setText("김예지");
+		profileMessage.setText("상태 메시지");
+	}
+
+	private void setButtons() {
+		btnEdit = findViewById(R.id.btn_edit);
+		btnEdit.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (mode == NORMAL_MODE) {
+					setEditMode();
+
+					editName.setText(profileName.getText().toString());
+					editMessage.setText(profileMessage.getText().toString());
+				}
+				else {
+					setNormalMode();
+
+					profileName.setText(editName.getText().toString());
+					profileMessage.setText(editMessage.getText().toString());
+				}
+			}
+		});
+
+		btnGoBefore = findViewById(R.id.btn_go_before);
 		btnGoBefore.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
@@ -45,6 +92,63 @@ public class SettingsActivity extends BaseActivity {
 				onClickUnlink();
 			}
 		});
+
+		btnCancel = findViewById(R.id.btn_cancel);
+		btnCancel.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				// dialog 보여주기
+				setNormalMode();
+			}
+		});
+
+		CircleImageView profileImg = findViewById(R.id.profile_img);
+		profileImg.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+			}
+		});
+
+		profileName = findViewById(R.id.text_name);
+		profileMessage = findViewById(R.id.text_message);
+		editName = findViewById(R.id.edit_name);
+		editMessage = findViewById(R.id.edit_message);
+		buttons = findViewById(R.id.buttons);
+	}
+
+	private void setNormalMode() {
+		mode = NORMAL_MODE;
+
+		btnGoBefore.setVisibility(View.VISIBLE);
+		btnCancel.setVisibility(View.INVISIBLE);
+
+		String strEdit = "Edit";
+		btnEdit.setText(strEdit);
+
+		buttons.setVisibility(View.VISIBLE);
+
+		profileName.setVisibility(View.VISIBLE);
+		profileMessage.setVisibility(View.VISIBLE);
+		editName.setVisibility(View.GONE);
+		editMessage.setVisibility(View.GONE);
+	}
+
+	private void setEditMode() {
+		mode = EDIT_MODE;
+
+		btnGoBefore.setVisibility(View.INVISIBLE);
+		btnCancel.setVisibility(View.VISIBLE);
+
+		String strDone = "Done";
+		btnEdit.setText(strDone);
+
+		buttons.setVisibility(View.INVISIBLE);
+
+		profileName.setVisibility(View.GONE);
+		profileMessage.setVisibility(View.GONE);
+		editName.setVisibility(View.VISIBLE);
+		editMessage.setVisibility(View.VISIBLE);
 	}
 
 	private void onClickLogout() {
