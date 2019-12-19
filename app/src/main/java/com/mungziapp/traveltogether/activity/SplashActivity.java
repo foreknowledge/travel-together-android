f887901219de48473641a014e5b13812d3c04bd5
s2choco <kyj_0129@naver.com>
2019-12-19 21:21:17 +0900

    splash 화면 성능 개선 & status bar 화면 별 색상 변경 & 내부화면 scroll 적용

--- app/src/main/java/com/mungziapp/traveltogether/activity/SplashActivity.java
+++ app/src/main/java/com/mungziapp/traveltogether/activity/SplashActivity.java
@@ -4,29 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
-import com.mungziapp.traveltogether.R;
 import com.mungziapp.traveltogether.app.TravelHelper;
 
 public class SplashActivity extends AppCompatActivity {
-	private Handler handler = new Handler();
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash);
 
 		TravelHelper.init(getApplicationContext());
-		final Intent intent = new Intent(getApplicationContext(), MainActivity.class);
 
-		handler.postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				//Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-				startActivity(intent);
-				finish();
-			}
-		}, 1000);
+		Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+		startActivity(intent);
+		finish();
 	}
 }
