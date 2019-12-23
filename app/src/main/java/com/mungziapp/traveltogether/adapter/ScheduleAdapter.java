edf4cf623701a7193ae9c8c8e2195c4c2ac7e49b
s2choco <kyj_0129@naver.com>
2019-12-23 23:58:51 +0900

    일정 recycler view 기능 구현 #8

--- app/src/main/java/com/mungziapp/traveltogether/adapter/ScheduleAdapter.java
+++ app/src/main/java/com/mungziapp/traveltogether/adapter/ScheduleAdapter.java
@@ -4,19 +4,20 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mungziapp.traveltogether.R;
-import com.mungziapp.traveltogether.data.TravelData;
 import com.mungziapp.traveltogether.interfaces.OnItemClickListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ViewHolder> {
 	private Context context;
-	private ArrayList<TravelData> travelData = new ArrayList<>();
+	private List<List<String>> dates = new ArrayList<>();
 
 	private OnItemClickListener listener;
 
@@ -24,9 +25,9 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ViewHo
 		this.context = context;
 	}
 
-	public void addItem(TravelData item) { travelData.add(item); }
-	public TravelData getItem(int position) {
-		return travelData.get(position);
+	public void addDate(List<String> date) { dates.add(date); }
+	public List<String> getDate(int position) {
+		return dates.get(position);
 	}
 	public void setClickListener(OnItemClickListener listener) {
 		this.listener = listener;
@@ -38,31 +39,44 @@ public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ViewHo
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View itemView = inflater.inflate(R.layout.item_schedule, parent, false);
 
-		return new ViewHolder(itemView, listener, context);
+		return new ViewHolder(itemView, listener);
 	}
 
 	@Override
 	public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-		holder.setItem(travelData.get(position));
+		holder.setItem(dates.get(position));
 	}
 
 	@Override
 	public int getItemCount() {
-		return travelData.size();
+		return dates.size();
 	}
 
 	static class ViewHolder extends RecyclerView.ViewHolder {
-		private Context context;
+		private TextView textDayN;
+		private TextView textDate;
 
-		ViewHolder(@NonNull final View itemView, final OnItemClickListener listener, final Context context) {
+		ViewHolder(@NonNull final View itemView, final OnItemClickListener listener) {
 			super(itemView);
 
-			this.context = context;
+			textDayN = itemView.findViewById(R.id.text_day_n);
+			textDate = itemView.findViewById(R.id.text_date);
+
+			TextView addSchedule = itemView.findViewById(R.id.btn_add_schedule);
+			addSchedule.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					if (listener != null) listener.onItemClick(ViewHolder.this, itemView, getAdapterPosition());
+				}
+			});
 
 		}
 
-		void setItem(TravelData item) {
-
+		void setItem(List<String> date) {
+			String strDayN = "DAY " + date.get(0);
+			textDayN.setText(strDayN);
+			String strDate = "(" + date.get(1) + ")";
+			textDate.setText(strDate);
 		}
 	}
 }
--- app/src/main/java/com/mungziapp/traveltogether/fragment/ScheduleFragment.java
+++ app/src/main/java/com/mungziapp/traveltogether/fragment/ScheduleFragment.java
@@ -1,11 +1,13 @@
 package com.mungziapp.traveltogether.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,8 +16,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mungziapp.traveltogether.R;
+import com.mungziapp.traveltogether.activity.AddScheduleActivity;
 import com.mungziapp.traveltogether.adapter.ScheduleAdapter;
 import com.mungziapp.traveltogether.interfaces.ActivityCallback;
+import com.mungziapp.traveltogether.interfaces.OnItemClickListener;
+
+import java.util.Arrays;
 
 public class ScheduleFragment extends Fragment {
 	private View rootView;
@@ -54,6 +60,28 @@ public class ScheduleFragment extends Fragment {
 		RecyclerView scheduleRecycler = rootView.findViewById(R.id.schedule_recycler);
 		scheduleRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
 
-		ScheduleAdapter scheduleAdapter = new ScheduleAdapter(getContext());
+		final ScheduleAdapter scheduleAdapter = new ScheduleAdapter(getContext());
+		scheduleAdapter.addDate(Arrays.asList("1", "2020.01.02"));
+		scheduleAdapter.addDate(Arrays.asList("2", "2020.01.03"));
+		scheduleAdapter.addDate(Arrays.asList("3", "2020.01.04"));
+		scheduleAdapter.addDate(Arrays.asList("4", "2020.01.20"));
+		scheduleAdapter.addDate(Arrays.asList("5", "2020.01.05"));
+		scheduleAdapter.addDate(Arrays.asList("6", "2020.01.06"));
+		scheduleAdapter.addDate(Arrays.asList("7", "2020.01.07"));
+
+		scheduleAdapter.setClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(RecyclerView.ViewHolder viewHolder, View view, int position) {
+				Intent intent = new Intent(getContext(), AddScheduleActivity.class);
+				startActivity(intent);
+			}
+
+			@Override
+			public Boolean onItemLongClick(RecyclerView.ViewHolder viewHolder, View view, int position) {
+				return null;
+			}
+		});
+
+		scheduleRecycler.setAdapter(scheduleAdapter);
 	}
 }
